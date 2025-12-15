cffdef441040219e957b643817778a6d529593e0
KDY0829 <idy970829@gmail.com>
2025-12-15 15:52:57 +0900

    사진 이미지 압축 기능 추가

--- src/controllers/postController.mjs
+++ src/controllers/postController.mjs
@@ -1,7 +1,11 @@
 // 포스트 관련 함수
 
 import db from "../config/db.mjs";
+import sharp from "sharp"; // 이미지 처리 라이브러리
+import fs from "fs/promises";
+import path from "path";
 
+// F004: 일반 게시물 작성
 export const createPost = async (req, res) => {
   try {
     if (!req.file) {
@@ -9,8 +13,27 @@ export const createPost = async (req, res) => {
     }
 
     const { content, postType, isSeniorMode } = req.body;
-    const imageUrl = `/uploads/${req.file.filename}`;
     const authorId = 2; // 임시
+
+    // 이미지 압축
+    const originalFilePath = req.file.path;
+
+    const optimizedFilename = `opt_${req.file.filename}`;
+    const optimizedFilePath = path.join(
+      req.file.destination,
+      optimizedFilename
+    );
+
+    // sharp 실행(1080px로 리사이징, 품질 80%로 압축)
+    await sharp(originalFilePath)
+      .resize({ width: 1080 }) // sns 표준너비
+      .withMetadata() // 사진의 방향 정보 유지
+      .jpeg({ quality: 80 }) // 화질 80%로 압축
+      .toFile(optimizedFilePath); // 저장
+
+    await fs.unlink(originalFilePath); // 원본 파일 삭제
+
+    const imageUrl = `/uploads/${optimizedFilename}`;
 
     const sql = `INSERT INTO posts (author_id, content, image_url, post_type, is_senior_mode) VALUES (?, ?, ?, ?, ?)`;
     const params = [authorId, content, imageUrl, isSeniorMode || false];
