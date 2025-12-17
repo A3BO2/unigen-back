c49c3d081a803adf0af81b777ff39d9817cf42b0
yujin <jinjini000@naver.com>
2025-12-17 13:34:04 +0900

    홈피드 전체 게시물 불러오기

--- src/router/postRouter.mjs
+++ src/router/postRouter.mjs
@@ -1,8 +1,11 @@
 // 게시물 라우터
-import express from 'express';
-import { createPost } from '../controllers/postController.mjs';
+import express from "express";
+import { createPost, getPost } from "../controllers/postController.mjs";
 const router = express.Router();
 
-router.post('/', createPost);
+router.post("/", createPost);
+
+// https://api.seniorsns.com/api/v1/posts/feed?mode=senior&page=1&size=10
+router.get("/feed", getPost);
 
 export default router;
