ab6f0466c61613a8e717b688c587f760d063ae40
soominn <chosm0129@naver.com>
2025-12-24 11:26:14 +0900

    스토리 S3 업로드 및 조회 기능 구현

--- src/router/postRouter.mjs
+++ src/router/postRouter.mjs
@@ -4,7 +4,7 @@ import {
   createPost,
   getReel,
   getFeed,
-  getStory,
+  getSeniorFeed,
 } from "../controllers/postController.mjs";
 import { upload } from "../middleware/uploadMiddleware.mjs";
 import { verifyToken } from "../middleware/authMiddleware.mjs";
@@ -19,6 +19,6 @@ router.get("/feed", verifyToken, getFeed);
 
 router.get("/reels", verifyToken, getReel);
 
-router.get("/stories", verifyToken, getStory);
+router.get("/seniorFeed", verifyToken, getSeniorFeed);
 
 export default router;
